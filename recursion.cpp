dba46c1a0c26727ab770857919c79c2e979e83e1
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-01-04 00:26:23 +0530

    Create recursion.cpp

---
+++ recursion.cpp
@@ -0,0 +1,16 @@
+### We Create Function IN recursion ###
+
+#include <iostream>
+using namespace std;
+
+void funA(){
+        cout << "A...." << endl;
+        }
+void funB(){
+        cout <<"B...." << endl;
+        }
+        
+int main()
+{
+ funA();   
+}
