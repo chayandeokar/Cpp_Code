5e6c35eb66de8ff0097959154ee9824ba2904775
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-01-10 00:50:21 +0530

    Update firstcode.c

--- firstcode.c
+++ firstcode.c
@@ -1,8 +1,18 @@
-
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
     int x=5;
-    
     printf("%d" , x);
 } 
+
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d" , &x);
+    printf("%d" , x);
+}
